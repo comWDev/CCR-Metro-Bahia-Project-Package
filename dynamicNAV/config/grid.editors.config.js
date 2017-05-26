25c0d28fab9b3b9da3cf0f83c3af42c036ea0c6e
comWDev <comWDev@gmail.com>
2017-05-26 19:15:09 -0300

    leblender progress but some sort of error when publishing content

--- dynamicNAV/config/grid.editors.config.js
+++ dynamicNAV/config/grid.editors.config.js
@@ -1,28 +1,28 @@
-﻿[
-	{
-	    "name": "Rich text editor",
-	    "alias": "rte",
-	    "view": "rte",
-	    "icon": "icon-article"
-	},
-	{
-	    "name": "Image",
-	    "alias": "media",
-	    "view": "media",
-	    "icon": "icon-picture"
-	},
-	{
-	    "name": "Macro",
-	    "alias": "macro",
-	    "view": "macro",
-	    "icon": "icon-settings-alt"
-	},
-	{
-	    "name": "Embed",
-	    "alias": "embed",
-	    "view": "embed",
-	    "icon": "icon-movie-alt"
-	},
+[
+    {
+        "name": "Rich text editor",
+        "alias": "rte",
+        "view": "rte",
+        "icon": "icon-article"
+    },
+    {
+        "name": "Image",
+        "alias": "media",
+        "view": "media",
+        "icon": "icon-picture"
+    },
+    {
+        "name": "Macro",
+        "alias": "macro",
+        "view": "macro",
+        "icon": "icon-settings-alt"
+    },
+    {
+        "name": "Embed",
+        "alias": "embed",
+        "view": "embed",
+        "icon": "icon-movie-alt"
+    },
     {
         "name": "Headline",
         "alias": "headline",
@@ -33,14 +33,47 @@
             "markup": "<h1>#value#</h1>"
         }
     },
-	{
-	    "name": "Quote",
-	    "alias": "quote",
-	    "view": "textstring",
-	    "icon": "icon-quote",
-	    "config": {
-	        "style": "border-left: 3px solid #ccc; padding: 10px; color: #ccc; font-family: serif; font-style: italic; font-size: 18px",
-	        "markup": "<blockquote>#value#</blockquote>"
-	    }
-	}
+    {
+        "name": "Quote",
+        "alias": "quote",
+        "view": "textstring",
+        "icon": "icon-quote",
+        "config": {
+            "style": "border-left: 3px solid #ccc; padding: 10px; color: #ccc; font-family: serif; font-style: italic; font-size: 18px",
+            "markup": "<blockquote>#value#</blockquote>"
+        }
+    },
+    {
+        "name": "Carousel",
+        "alias": "carousel",
+        "view": "/App_Plugins/LeBlender/editors/leblendereditor/LeBlendereditor.html",
+        "icon": "icon-photo-album",
+        "render": "/App_Plugins/LeBlender/editors/leblendereditor/views/Base.cshtml",
+        "config": {
+            "editors": [
+                {
+                    "name": "Image",
+                    "alias": "image",
+                    "propretyType": {},
+                    "dataType": "135d60e0-64d9-49ed-ab08-893c9ba44ae5"
+                },
+                {
+                    "name": "Title",
+                    "alias": "title",
+                    "propretyType": {},
+                    "dataType": "0cc0eba1-9960-42c9-bf9b-60e150b429ae"
+                },
+                {
+                    "name": "Summary",
+                    "alias": "summary",
+                    "propretyType": {},
+                    "dataType": "0cc0eba1-9960-42c9-bf9b-60e150b429ae"
+                }
+            ],
+            "renderInGrid": "1",
+            "min": 1,
+            "max": 10,
+            "frontView": ""
+        }
+    }
 ]
