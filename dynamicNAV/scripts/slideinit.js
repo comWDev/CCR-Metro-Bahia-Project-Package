f97476f6eb0825e997325e4e217691d16865e551
kick-media-brasil <brett@kickmediabrasil.com>
2017-05-30 00:16:26 -0300

    Slider tabs resolved except null option statment

--- dynamicNAV/scripts/slideinit.js
+++ dynamicNAV/scripts/slideinit.js
@@ -1,14 +1,18 @@
 
     $(document).ready(function(){
         $('.slider-1').slick({
-      	prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
       nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
-      dots: true,
+      dots: false,
   infinite: true,
   autoplay: true,
   autoplaySpeed: 5500,
   speed: 1000,
   cssEase: 'cubic-bezier(.51,.01,.52,.99)',
+  centerMode: true,
+  centerPadding: '-30px',
+  slidesToShow: 3,
+  slidesToScroll: 3,
   arrows: true,
 
   responsive: [
@@ -16,21 +20,31 @@
       breakpoint: 1024,
       settings: {
         infinite: true,
+        centerMode: true,
+          centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 3,
         arrows: true,
-        dots: true
+        dots: false
       }
     },
     {
       breakpoint: 600,
       settings: {
         infinite: true,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 3,
         arrows: false,
-        dots: true
+        dots: false
       }
     },
     {
       breakpoint: 480,
       settings: {
+        centerMode: false,
+                slidesToShow: 1,
+        slidesToScroll: 3,
         arrows: false,
         dots:false,
       }
@@ -42,14 +56,18 @@
 
 });
         $('.slider-2').slick({
-      	prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
       nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
-      dots: true,
+      dots: false,
   infinite: true,
   autoplay: true,
   autoplaySpeed: 5500,
   speed: 1000,
   cssEase: 'cubic-bezier(.51,.01,.52,.99)',
+  centerMode: true,
+  centerPadding: '-30px',
+  slidesToShow: 3,
+  slidesToScroll: 3,
   arrows: true,
 
   responsive: [
@@ -57,144 +75,31 @@
       breakpoint: 1024,
       settings: {
         infinite: true,
+        centerMode: true,
+          centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 3,
         arrows: true,
-        dots: true
+        dots: false
       }
     },
     {
       breakpoint: 600,
       settings: {
         infinite: true,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 3,
         arrows: false,
-        dots: true
+        dots: false
       }
     },
     {
       breakpoint: 480,
       settings: {
-        arrows: false,
-        dots:false,
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-
-});
-        $('.slider-3').slick({
-      	prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
-      nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
-      dots: true,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 5500,
-  speed: 1000,
-  cssEase: 'cubic-bezier(.51,.01,.52,.99)',
-  arrows: true,
-
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        infinite: true,
-        arrows: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        infinite: true,
-        arrows: false,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        dots:false,
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-
-});
-        $('.slider-4').slick({
-      	prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
-      nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
-      dots: true,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 5500,
-  speed: 1000,
-  cssEase: 'cubic-bezier(.51,.01,.52,.99)',
-  arrows: true,
-
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        infinite: true,
-        arrows: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        infinite: true,
-        arrows: false,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        dots:false,
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-
-});
-      $('.slider-5').slick({
-      	prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
-      nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
-      dots: true,
-  infinite: true,
-  autoplay: true,
-  autoplaySpeed: 5500,
-  speed: 1000,
-  cssEase: 'cubic-bezier(.51,.01,.52,.99)',
-  arrows: true,
-
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        infinite: true,
-        arrows: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        infinite: true,
-        arrows: false,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
+        centerMode: false,
+                slidesToShow: 1,
+        slidesToScroll: 3,
         arrows: false,
         dots:false,
       }
@@ -236,6 +141,171 @@
     {
       breakpoint: 480,
       settings: {
+        arrows: false,
+        dots:false,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
+        $('.slider-3').slick({
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
+      nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
+      dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5500,
+  speed: 1000,
+  cssEase: 'cubic-bezier(.51,.01,.52,.99)',
+  centerMode: true,
+  centerPadding: '-30px',
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        infinite: true,
+        centerMode: true,
+          centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        infinite: true,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: false,
+                slidesToShow: 1,
+        slidesToScroll: 3,
+        arrows: false,
+        dots:false,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
+        $('.slider-4').slick({
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
+      nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
+      dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5500,
+  speed: 1000,
+  cssEase: 'cubic-bezier(.51,.01,.52,.99)',
+  centerMode: true,
+  centerPadding: '-30px',
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        infinite: true,
+        centerMode: true,
+          centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        infinite: true,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: false,
+                slidesToShow: 1,
+        slidesToScroll: 3,
+        arrows: false,
+        dots:false,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
+        $('.slider-5').slick({
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
+      nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
+      dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5500,
+  speed: 1000,
+  cssEase: 'cubic-bezier(.51,.01,.52,.99)',
+  centerMode: true,
+  centerPadding: '-30px',
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        infinite: true,
+        centerMode: true,
+          centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        infinite: true,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: false,
+                slidesToShow: 1,
+        slidesToScroll: 3,
         arrows: false,
         dots:false,
       }
