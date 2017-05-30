e45cc1140360536ff9452c2f95211ac3ff7a1299
comWDev <comWDev@gmail.com>
2017-05-30 10:14:18 -0300

    slider tab code modified but still showing null value error

--- dynamicNAV/scripts/slideinit.js
+++ dynamicNAV/scripts/slideinit.js
@@ -1,5 +1,60 @@
 
     $(document).ready(function(){
+         $('.slider-x').slick({
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
+      nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
+      dots: false,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 5500,
+  speed: 1000,
+  cssEase: 'cubic-bezier(.51,.01,.52,.99)',
+  centerMode: true,
+  centerPadding: '-30px',
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  arrows: true,
+
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        infinite: true,
+        centerMode: true,
+          centerPadding: '0px',
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        infinite: true,
+        centerMode: false,
+        slidesToShow: 1,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: false,
+                slidesToShow: 1,
+        slidesToScroll: 3,
+        arrows: false,
+        dots:false,
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
         $('.slider-1').slick({
         prevArrow:"<img class='a-left control-c prev slick-prev' src='/media/1012/prev_60x60.png'>",
       nextArrow:"<img class='a-right control-c next slick-next' src='/media/1013/next_60x60.png'>",
