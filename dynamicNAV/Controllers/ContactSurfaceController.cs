5d653fbb13cbfdaa3a1372e0c70c3d7e5deb0278
comWDev <comWDev@gmail.com>
2017-06-19 18:52:58 -0300

    contact form controller, model and view all created need to complete and test -- pickup at 23:26

---
+++ dynamicNAV/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,39 @@
+﻿using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using dynamicNAV.Models;
+using System.Net.Mail;
+
+namespace dynamicNAV.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Contact/";
+
+        public ActionResult RenderForm()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Contact.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(ContactModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                SendEmail(model);
+                return RedirectToCurrentUmbracoPage();
+            }
+            return CurrentUmbracoPage();
+        }
+
+        private void SendEmail(ContactModel model)
+        {
+            MailMessage message = new MailMessage(model.EmailAddress, "comwdev@gmail.com");
+            message.Subject = string.Format("Inquiry from {0} {1} - {2}", model.FirstName, model.LastName, model.EmailAddress);
+            message.Body = model.Message;
+            SmtpClient client = new SmtpClient("127.0.0.1", 25);
+            client.Send(message);
+        }
+    }
+}
---
+++ dynamicNAV/Models/ContactModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dynamicNAV.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
